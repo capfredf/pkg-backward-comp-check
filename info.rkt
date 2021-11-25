227a6649a3fa7b14dd8e3ff7741892c36b97b38b
Fred Fu <moonsolo@gmail.com>
2021-11-25 15:54:44 -0500

    add an info file

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define name "package backward compatibility check")
+(define collection "pkg-bc-check")
+(define version "0.1")
+(define deps '(raco-static-web package-analysis rebellion remote-shell pkg-build))
+(define build-deps '())
+(define pkg-authors '(moonsolo@gmail.com))
