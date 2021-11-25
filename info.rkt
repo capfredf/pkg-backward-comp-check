080601bf5b0362e6d6e17f61c908668d07b5cc82
Fred Fu <moonsolo@gmail.com>
2021-11-25 16:20:19 -0500

    add a raco subcommand

--- info.rkt
+++ info.rkt
@@ -6,3 +6,9 @@
 (define deps '(raco-static-web package-analysis rebellion remote-shell pkg-build))
 (define build-deps '())
 (define pkg-authors '(moonsolo@gmail.com))
+
+(define raco-commands
+  '(("pkg-bcc"
+     (submod pkg-bc-check main)
+     "checks the backward compatibility of a change to Racket or base packages"
+     10)))
